08051edb4cf13831f6ea6d52e536541ce8cd51d0
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2022-06-18 16:43:54 -0300

    form 02
    002

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'towgov';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+       
+
+?>
